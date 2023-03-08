f6830717db2409c8cdaacc9ba66f5382edf9ffe7
Carlos Alberto López Mata <mata66666666666@gmail.com>
2023-03-08 13:01:09 -0600

    Updates

---
+++ precache-manifest.0f602996e32b262dbd1e68bac22ef204.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0017e42ae587d5dda1c",
+    "url": "/Cv_CALM_React_Typescript/static/css/main.0f0532e1.chunk.css"
+  },
+  {
+    "revision": "c0017e42ae587d5dda1c",
+    "url": "/Cv_CALM_React_Typescript/static/js/main.c0017e42.chunk.js"
+  },
+  {
+    "revision": "766be002cfdbb69832ec",
+    "url": "/Cv_CALM_React_Typescript/static/js/1.766be002.chunk.js"
+  },
+  {
+    "revision": "36fa21d7c0468ee463dc",
+    "url": "/Cv_CALM_React_Typescript/static/js/2.36fa21d7.chunk.js"
+  },
+  {
+    "revision": "f75753aa6508e91a9df6",
+    "url": "/Cv_CALM_React_Typescript/static/js/runtime~main.f75753aa.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Cv_CALM_React_Typescript/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "edd797d95d6c79ed027231ce24d837f3",
+    "url": "/Cv_CALM_React_Typescript/index.html"
+  }
+];
